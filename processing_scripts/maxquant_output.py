d70aa86ce5caa51f0fabe60af6544f7fbf5a07a0
Hazel Dilmore <hazel.dilmore@czbiohub.org>
2022-08-04 16:33:02 -0700

    cleaned up code with new wrapper

--- processing_scripts/maxquant_output.py
+++ processing_scripts/maxquant_output.py
@@ -1,49 +1,27 @@
 def expand_column(df, col, sep=';'):
-    
-    '''Helper function to expand columns, which turns out to be common'''
+    '''Helper function to expand columns that contain lists separated by semicolons'''
     return df[col].str.split(sep, expand=True).stack.reset_index()
 
 def peptide_nodes(peptide_seqs):
-    
     '''Helper function to make the .csv file that creates :Peptide nodes.'''
     unique_peptides = peptide_seqs['peptide_sequence'].unique()
     peptide_df = pd.DataFrame(data={'peptide_sequence': unique_peptides})
     peptide_df.rename_axis(index='peptide_id', inplace=True)
     return peptide_df
 
-def maxquant_inference(path, experiment_id):
-    '''Loads the maxquant proteinGroups file at the given file path and creates a relationship
-    mapping protein ids to peptide ids. This is the raw mapping given by MaxQuant (is not well 
-    validated or curated).'''
-    df = pd.read_table(path)
-    # ENH: check that df is of expected format
-    
-    # expand the protein and peptide ID mapping
-    protein_ids = df['Protein IDs'].str.split(';', expand=True).stack().reset_index()
-    peptide_seq = df['Peptide sequences'].str.split(';', expand=True).stack().reset_index()
-    
-    # rename columns to more accurately reflect what they are 
-    protein_ids.rename(columns={0:'uniprot_id'}, inplace=True)
-    peptide_seq.rename(columns={0:'peptide_sequence'}, inplace=True)
-    
+def maxquant_inference(protein_ids, peptide_seq):
+    '''Creates a relationship mapping protein IDs to peptide IDs. This is the raw mapping 
+    given by MaxQuant (is not well validated or curated).'''
     # inner join on level_0 which represents original index of df
     merged = peptide_seq[['level_0', 'peptide_sequence']].merge(protein_ids[['level_0', 'uniprot_id']],
 								right_on='level_0', left_on='level_0')
-    merged['experiment_id'] = experiment_id
-    return merged[['uniprot_id', 'peptide_sequence', 'experiment_id']]
+    return merged[['uniprot_id', 'peptide_sequence']]
 
-def maxquant_quantification(path):
+def maxquant_quantification(df, protein_ids):
     '''Loads the maxquant proteinGroups file at the given file path and creates a relationship
     mapping sample names to uniprot IDs, and keep LFQ intensity as an attribute. This is not the 
     ideal relationship (in the future, we will have another inference engine translate this, not 
     MaxQuant).'''
-    df = pd.read_table(path)
-    # ENH check that df is of expected path 
-    
-    # expand the protein ID mapping
-    protein_ids = df['Protein IDs'].str.split(';', expand=True).stack().reset_index()
-    protein_ids.rename(columns={0:'uniprot_id'}, inplace=True)
-    
     # pull out the unique sample names in this experiment 
     sample_names = [name[9:] for name in df.columns[df.columns.str.contains('Peptides ')]]
     
