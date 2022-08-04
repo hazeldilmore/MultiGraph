4ca73bee045a9e3d560ac5235b74a630104ac758
Hazel Dilmore <hazel.dilmore@czbiohub.org>
2022-08-04 14:35:35 -0700

    added maxquant inference relationship

---
+++ processing_scripts/maxquant_output.py
@@ -0,0 +1,20 @@
+def maxquant_inference(path, experiment_id):
+    '''Loads the maxquant proteinGroups file at the given file path and creates a relationship
+    mapping protein ids to peptide ids. This is the raw mapping given by MaxQuant (is not well 
+    validated or curated).'''
+    df = pd.read_table(path)
+    # ENH: check that df is of expected format
+    
+    # expand the protein and peptide ID mapping
+    protein_ids = df['Protein IDs'].str.split(';', expand=True).stack().reset_index()
+    peptide_ids = df['Peptide IDs'].str.split(';', expand=True).stack().reset_index()
+    
+    # rename columns to more accurately reflect what they are 
+    protein_ids.rename(columns={0:'uniprot_id'}, inplace=True)
+    peptide_ids.rename(columns={0:'peptide_id'}, inplace=True)
+    
+    # inner join on level_0 which represents original index of df
+    merged = peptide_ids[['level_0', 'peptide_id']].merge(protein_ids[['level_0', 'uniprot_id']],
+                                                          right_on='level_0', left_on='level_0')
+    merged['experiment_id'] = experiment_id
+    return merged[['uniprot_id', 'peptide_id', 'experiment_id']]
